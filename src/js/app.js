2b3af08add2c29ea823fc9471651cf06d7b47790
Chiko-Dev <chikobg12345@gmail.com>
2021-12-18 22:46:46 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,19 @@
 import "../scss/app.scss";
+import * as R from '/node_modules/ramda/es/index.js';
+
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const arrayToPluck = [
+  let arrayToPluck = R.pluck('class');
+  arrayToPluck = [
     { name: "John", class: "is-primary" },
     { age: 23, class: "is-warning" },
     { job: "programmer", class: "is-danger" },
   ];
+
+
   const articles = document.querySelectorAll("article");
+
+
 });
